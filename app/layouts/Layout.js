328b2a04313c0c0c98961dd340e3ced41968ed50
Ishimwe Jules Pacis (Mediator) <51479761+pacisjules@users.noreply.github.com>
2023-01-18 01:12:02 +0200

    Protect Dashboard page

--- app/layouts/Layout.js
+++ app/layouts/Layout.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import Topbar from '../components/Topbar.js';
+import React from "react";
+import Topbar from "../components/Topbar.js";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Layout = ({ children }) => {
+  const { data: session, status } = useSession({
+    required: true,
+  });
+
+  if (status === "authenticated") {
     return (
-        
-        <div> 
-            <Topbar/>
-            {children}
-        </div>
+      <div>
+        <Topbar />
+        {children}
+      </div>
     );
-}
+  }
+};
 
 export default Layout;
